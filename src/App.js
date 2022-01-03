6e724d433f4bd1906194600ce1a3859054527f16
Yh <c.yhkim17@gmail.com>
2022-01-03 18:51:33 +1300

    trying class out, but should revert back to function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '', apiResult: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>Bible Search 1.0</h3>
+
+      <form onSubmit={this.handleSubmit}>
+          <label>
+            Search for a verse?
+          </label>
+          <br></br>
+          <input
+            id="new-todo"
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.value}
+          />
+          <button>
+            Search
+          </button>
+      </form>
+
+      {this.state.apiResult ? <ul><li>{this.state.apiResult}</li></ul> : null}
+    
       </header>
     </div>
-  );
+    )
+  }
+    
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) { 
+    this.setState({ value:'', apiResult: this.state.value });
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
 }
 
 export default App;
